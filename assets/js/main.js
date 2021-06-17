401d8571f2f03046fc878b1801e546ce7e2bb2cc
Alexandre Berthon <alexandre@coorganiz.com>
2021-06-17 21:21:13 +0200

    temp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,73 @@
-function isScrolledIntoView(el) {
-    var rect = el.getBoundingClientRect();
-    var elemTop = rect.top;
-    var elemBottom = rect.bottom;
 
-    // Only completely visible elements return true:
-    //var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    return isVisible;
+function animateBars() {
+   var hT = $('.level-bar').first().offset().top,
+       hH = $('.level-bar').first().outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+   if (wS > (hT-wH)){
+     $('.level-bar-inner').each(function() {
+        var itemWidth = $(this).data('level');
+        $(this).animate({
+            width: itemWidth
+        }, 800); 
+    });
+   }
 }
+
+function getPDF(){
+
+  window.jsPDF = window.jspdf.jsPDF;
+
+  var HTML_Width = $(".wrapper").width();
+  var HTML_Height = $(".wrapper").height();
+  var top_left_margin = 0;
+  var PDF_Width = HTML_Width+(top_left_margin*2);
+  var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+  var canvas_image_width = HTML_Width;
+  var canvas_image_height = HTML_Height;
+  
+  var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+  
+
+  html2canvas($(".wrapper")[0],{allowTaint:true}).then(function(canvas) {
+    canvas.getContext('2d');
+    
+    console.log(canvas.height+"  "+canvas.width);
+    
+    
+    var imgData = canvas.toDataURL("image/jpeg", 1.0);
+    var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+      pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+    
+    
+    for (var i = 1; i <= totalPDFPages; i++) { 
+      pdf.addPage(PDF_Width, PDF_Height);
+      pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+    }
+    
+      pdf.save("HTML-Document.pdf");
+  });
+};
 
 jQuery(document).ready(function($) {
 
-    $('.level-bar-inner').css('width', '0');
+    // $('.level-bar-inner').css('width', '0');
     
-    $(window).on('load', function() {
+    // $(window).on('load', function() {
 
-    	$(window).scroll(function() {
-		   var hT = $('.level-bar').first().offset().top,
-		       hH = $('.level-bar').first().outerHeight(),
-		       wH = $(window).height(),
-		       wS = $(this).scrollTop();
-		    console.log((hT-wH) , wS);
-		   if (wS > (hT-wH)){
-		     $('.level-bar-inner').each(function() {
-	            var itemWidth = $(this).data('level');
-	            $(this).animate({
-	                width: itemWidth
-	            }, 800); 
-	        });
-		   }
-		});
+    //   animateBars();
 
-        
+    //   $(window).scroll(function() {
+    //     animateBars();
+    //   });
+    // });
 
-    });
-   
-    
+    // $('.save-pdf').click(function() {
+    //   getPDF();
+    // })
 
+    $('.level-bar-inner').each(function() {
+        var itemWidth = $(this).data('level');
+        $(this).css('width', itemWidth);
+      })
 });
